3bb62275d34e8dc8eb72dfbae8d4907d72904ce2
AC81555 <AC81555@IND-5CG6126QLS.ctl.intranet>
2020-12-07 15:43:46 +0530

    Refactored folder name

--- exercise/src/com/exercise/Application.java
+++ exercise/src/com/exercise/Application.java
@@ -3,6 +3,7 @@ package com.exercise;
 public class Application {
 
 	public static void main(String[] args) {
+		
 		MobileBill bill=new MobileBill("Anvita","87907647");
 		System.out.println("Name :"+bill.getCustomerName());
 		System.out.println("Mobile Number :"+bill.getMobileNumber());
